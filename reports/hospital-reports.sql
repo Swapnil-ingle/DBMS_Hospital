79a262d56769e344ce06205c751ea3ed1ac00f65
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2019-03-05 10:15:58 +0530

    SI: Tested and updated the hospital reports SQLs

--- reports/hospital-reports.sql
+++ reports/hospital-reports.sql
@@ -4,9 +4,9 @@ SELECT COUNT(*)
 FROM Hospitalization
 WHERE MONTH(hospitalized_on) = MONTH(current_timestamp);
 
-## Find the total Expenses incurred, its type, and who handled it for this month.
+## Find the total Expenses incurred, its type, and who handled it for this month, and expense details.
 
-SELECT Amount_Spent, s.Name as Supervised_By, et.name as Expense_Type
+SELECT Amount_Spent, s.Name as Supervised_By, et.name as Expense_Type, e.description as Expense_Description
 FROM Expenses e
 	JOIN Expense_Type et ON e.type_of_expense = et.Identifier
 	JOIN Staff s ON e.Supervised_By = s.Identifier
@@ -14,17 +14,15 @@ WHERE MONTH(e.Date) = MONTH(current_timestamp);
 
 ## Make a report of all staff leaves. (All staff info and how many leaves they’ve taken)
 
-SELECT s.name, s.Department, s.Position, s.Address, s.Contact_number, s.Email, COUNT(*) as Num_of_leaves
+SELECT s.name as Staff_Name, dep.name as Department, des.name as Designation, s.Address, s.Contact_number, s.Email, COUNT(*) as Num_of_leaves
 FROM Leaves l 
-	JOIN Staff s ON l.Staff_ID = s.Identifier
-GROUP BY s.name, s.Department, s.Position, s.Address, s.Contact_number, s.Email;
+	LEFT JOIN Staff s ON l.Staff_ID = s.Identifier
+	LEFT JOIN Departments dep ON s.department_id = dep.Identifier
+	LEFT JOIN Designations des ON s.designation_id = des.Identifier
+GROUP BY s.name, dep.name, des.name, s.Address, s.Contact_number, s.Email;
 
-### Same using sub-query
+## Find the number of rooms(and their relevant information) that are vacant.
 
-SELECT s.name, s.Department, s.Position, s.Address, s.Contact_number, s.Email, l.num_of_leaves
-FROM (SELECT COUNT(*) as num_of_leaves, Staff_ID
-		FROM Leaves 
-		GROUP BY Staff_ID
-		HAVING MONTH(DATE) = MONTH(current_timestamp)) l
-	JOIN Staff s ON l.Staff_ID = s.Identifier;
-
+# SELECT 
+# FROM Hospitalization h
+#	RIGHT OUTER JOIN Room r ON h.Room_Allocated = r.Identifier
